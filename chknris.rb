37540ce3396fc08e3ed4cbee929e2c524be88794
xRuiAlves <up201606746@fe.up.pt>
2019-07-26 22:06:58 +0100

    Add fetch quote by category

--- chknris.rb
+++ chknris.rb
@@ -1,10 +1,11 @@
-require 'net/http'
-require 'json'
+require "net/http"
+require "json"
 
 $version = "1.0"
 
 
 class ChuckFailedUs < StandardError; end
+class ChuckIsOblivious < StandardError; end
 
 
 def print_help
@@ -19,6 +20,7 @@ def print_help
     puts "\t-h, --help\tprints cli help"
     puts "\t-v, --version\tprints version information"
     puts "\t-l, --list\tlists chuck norris quotes categories"
+    puts "\t-c, --category\tspecify chuck norris quote category"
 end
 
 
@@ -48,6 +50,22 @@ def parse_args
             STDERR.puts "Failed to fetch quote categories list"
             exit 2
         end
+    when "-c", "--category"
+        category = ARGV[1]
+        if !category
+            STDERR.puts "Quote category missing!"
+            exit 1
+        end
+        begin
+            puts get_quote(category)
+        rescue ChuckIsOblivious
+            STDERR.puts "Quote category is invalid.\n\n"
+            STDERR.puts "Use 'chrnris --list' to obtain the full list of valid quote categories."
+            exit 1
+        rescue ChuckFailedUs
+            STDERR.puts "Failed to fetch quote"
+            exit 2
+        end
     else
         STDERR.puts "Invalid flag \"#{ARGV[0]}\"\n\n"
         bad_arguments()
@@ -56,7 +74,7 @@ end
 
 
 def get_random_quote
-    res = Net::HTTP.get_response(URI('https://api.chucknorris.io/jokes/random'))
+    res = Net::HTTP.get_response(URI("https://api.chucknorris.io/jokes/random"))
     if res.code != "200"
         raise ChuckFailedUs
     end
@@ -65,11 +83,22 @@ end
 
 
 def get_quote_categories
-    res = Net::HTTP.get_response(URI('https://api.chucknorris.io/jokes/categories'))
+    res = Net::HTTP.get_response(URI("https://api.chucknorris.io/jokes/categories"))
     if res.code != "200"
         raise ChuckFailedUs
     end
     JSON.parse(res.body)
+end
+
+
+def get_quote(category)
+    res = Net::HTTP.get_response(URI("https://api.chucknorris.io/jokes/random?category=#{category}"))
+    if res.code == "404"
+        raise ChuckIsOblivious
+    elsif res.code != "200"
+        raise ChuckFailedUs
+    end
+    JSON.parse(res.body)["value"]
 end
 
 
