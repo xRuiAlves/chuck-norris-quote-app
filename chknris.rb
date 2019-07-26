3ab1e777faf0365d411901389a518552818bb998
xRuiAlves <up201606746@fe.up.pt>
2019-07-26 21:16:52 +0100

    Add flags parsing

--- chknris.rb
+++ chknris.rb
@@ -1,8 +1,43 @@
 $version = "1.0"
 
-def help
+
+def print_help
     puts "Chuck Norris quotes cli app"
-    puts "version: v#{$version}\n"
+    print "version: "
+    print_version()
+
+    puts "\nUSAGE"
+    puts "\t-h, --help\tprints cli help"
+    puts "\t-v, --version\tprints version information"
 end
 
-help()
+
+def print_version
+    puts "v#{$version}"
+end
+
+
+def bad_arguments
+    print_help()
+    exit 1
+end
+
+
+def parse_args
+    case ARGV[0]
+    when "-h", "--help"
+        print_help()
+    when "-v", "--version"
+        print_version()
+    else
+        bad_arguments()
+    end
+end
+
+
+if ARGV.length == 0
+    bad_arguments()
+end
+
+
+parse_args()
